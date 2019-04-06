f12280db26d193f66b04185ca201b562968b4221
keithagroves <keith.groves@jointheleague.org>
2019-04-05 20:04:17 -0700

    kj

---
+++ src/test/Kata.java
@@ -0,0 +1,8 @@
+package test;
+public class Kata
+{
+    public static int[] countPositivesSumNegatives(int[] input)
+    {
+        return null; //return an array with count of positives and sum of negatives
+    }
+}
